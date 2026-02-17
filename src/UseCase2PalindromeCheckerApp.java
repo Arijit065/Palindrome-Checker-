1b27877169231ca3083cb7c49cf076a0bd439b8a
Arijit065 <arijitmandal065@gmail.com>
2026-02-17 10:18:20 +0530

    Added Palindrome Checker App

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+public  class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String originalStr = "madam";
+        String reversedStr = "";
+
+        System.out.println("Checking string: " + originalStr);
+
+        for (int i = originalStr.length() - 1; i >= 0; i--) {
+            reversedStr = reversedStr + originalStr.charAt(i);
+        }
+
+        if (originalStr.equals(reversedStr)) {
+            System.out.println("Result: The string is a Palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a Palindrome.");
+        }
+    }
+}
